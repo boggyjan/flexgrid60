f7f2e4b580a4f4c8f9f19c5b0a5787ae9f461c01
Boggy Jang <boggyjan@gmail.com>
2016-11-30 14:38:37 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+'use strict';
+
+// requires
+var gulp         = require('gulp');
+var connect      = require('gulp-connect');
+var ejs          = require('gulp-ejs');
+var rmHtmlComm   = require('gulp-remove-html-comments');
+var rmEmptyLn    = require('gulp-remove-empty-lines');
+var prettify     = require('gulp-html-prettify');
+var htmlhint     = require("gulp-htmlhint");
+var imagemin     = require('gulp-imagemin');
+var sourcemaps   = require('gulp-sourcemaps');
+var rename       = require('gulp-rename');
+var gutil        = require('gulp-util');
+var del          = require('del');
+var sass         = require('gulp-sass');
+var postcss      = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+// variables
+var watching     = false;
+
+// error handler
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
+
+// tasks
+gulp.task('clean', function(cb) {
+  return del([!watching ? 'dist' : '']);
+});
+
+gulp.task('connect', ['clean'], function() {
+  connect.server({
+    root: 'dist/',
+    port: 8888,
+    livereload: true
+  });
+});
+
+gulp.task('ejs', ['clean'], function () {
+  return gulp.src('*.ejs')
+        .pipe(ejs({
+          // global vars
+        }).on('error', gutil.log))
+        .pipe(htmlhint())
+        .pipe(htmlhint.reporter('htmlhint-stylish'))
+        .pipe(htmlhint.failReporter({
+          supress: true
+        }).on('error', handleError))
+        .pipe(rename({extname: '.html'}))
+        .pipe(rmHtmlComm())
+        .pipe(rmEmptyLn())
+        .pipe(prettify({indent_inner_html: true, indent_char: ' ', indent_size: 2, unformatted: ['pre', 'code']}))
+        .pipe(gulp.dest('dist'))
+        .pipe(connect.reload());
+});
+
+gulp.task('css', ['clean'], function() {
+  var processors = [
+    autoprefixer({browsers: ['last 3 versions']})
+  ];
+
+  return gulp.src(['scss/*.scss', '!scss/_*.scss'])
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'})
+        .on('error', sass.logError))
+        .pipe(postcss(processors))
+        .on('error', handleError)
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('default',    ['clean', 'connect', 'ejs', 'css', 'watch']);
+gulp.task('watch', function() {
+  watching = true;
+  gulp.watch(['*.ejs'], ['ejs']);
+  gulp.watch('scss/*.scss', ['css']);
+});
---
+++ readme.md
@@ -0,0 +1,27 @@
+# FlexGrid 60
+
+## Usage
+```html
+<link rel="stylesheet" href="http://boggyjan.github.io/flexgrid60/dist/css/flexgrid60.css">
+```
+
+
+## Grid options
+|                 | Extra small devices Phones (<768px) | Small devices Tablets (≥768px) | Medium devices Desktops (≥992px) | Large devices Desktops (≥1200px) |
+|-----------------|-------------------------------------|--------------------------------|----------------------------------|----------------------------------|
+| Class prefix    | .fx-xs-                             | .fx-sm-                        | .fx-md-                          | .fx-lg-                          |
+| # of columns    | 60                                  | 60                             | 60                               | 60                               |
+| Gutter width    | 0                                   | 0                              | 0                                | 0                                |
+| Nestable        | Yes                                 | Yes                            | Yes                              | Yes                              |
+| Offsets         | No                                  | No                             | No                               | No                               |
+| Column ordering | No                                  | No                             | No                               | No                               |
+
+
+## Example
+```html
+<div class="fx-row">
+  <div class="fx-lg-10 fx-sm-20 fx-xs-30">...</div>
+  <div class="fx-lg-20 fx-sm-20 fx-xs-30">...</div>
+  <div class="fx-lg-30 fx-sm-20 fx-xs-60">...</div>
+</div>
+```
